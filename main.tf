3ca9b9b4c8292b7a47bc5d0c277606389fe17177
Angad Wagh <waghangad@gmail.com>
2025-05-01 10:56:17 +0000

    bucket change

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "tf-state-987654321"
+    bucket         = "tf-state-987654328"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "tf.lock"

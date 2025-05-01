6db617a2a5273db04a1826f7ec85031cec41c83f
Angad Wagh <waghangad@gmail.com>
2025-05-01 09:19:16 +0000

    commit

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-aws
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-987654321"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf.lock"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}

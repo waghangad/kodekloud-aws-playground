d8b6dae8d0c50a643caa3326ae16f96ecca9ff1b
Angad Wagh <waghangad@gmail.com>
2025-05-02 06:30:37 +0000

    public only

--- main.tf
+++ main.tf
@@ -33,7 +33,6 @@ module "public_ecr" {
   source = "terraform-aws-modules/ecr/aws"
   repository_name = "study/demo-repo"
   repository_type = "public"
-  repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
 }
 
 

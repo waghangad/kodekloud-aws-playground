aaf97d3b79c569c7978d67280e90fb6584cc3b3f
Angad Wagh <waghangad@gmail.com>
2025-05-02 06:26:59 +0000

    commit

--- main.tf
+++ main.tf
@@ -31,25 +31,9 @@ resource "aws_s3_bucket" "create_s3_bucket" {
 
 module "public_ecr" {
   source = "terraform-aws-modules/ecr/aws"
-
   repository_name = "study/demo-repo"
   repository_type = "public"
-
   repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
-
-  public_repository_catalog_data = {
-    description       = "Docker container for some things"
-    about_text        = file("${path.module}/files/ABOUT.md")
-    usage_text        = file("${path.module}/files/USAGE.md")
-    operating_systems = ["Linux"]
-    architectures     = ["x86"]
-    logo_image_blob   = filebase64("${path.module}/files/clowd.png")
-  }
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
 }
 
 

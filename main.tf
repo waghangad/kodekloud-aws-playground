86a1214316e1d976a5fa3539dd050e4c03926b3c
Angad Wagh <waghangad@gmail.com>
2025-05-02 06:07:48 +0000

    visibility public

--- main.tf
+++ main.tf
@@ -22,6 +22,7 @@ resource "aws_s3_bucket" "create_s3_bucket" {
 resource "aws_ecr_repository" "create_ecr_repo" {
   name = "study/demo-repo"
   image_tag_mutability = "IMMUTABLE"
+  visibility = "public"
   image_scanning_configuration {
     scan_on_push = false
   }

4c0e2c5c6979f6286553c76e49cf4cc88d6a4bf1
Angad Wagh <waghangad@gmail.com>
2026-04-03 12:52:14 +0530

    new account created

--- eks/main.tf
+++ eks/main.tf
@@ -6,7 +6,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "tf-state-eks-bucket-654654499176-us-east-1"
+    bucket         = "tf-state-eks-bucket-637423240208-us-east-1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "tf.lock"
